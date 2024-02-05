56b93aed67ff75ee974dc2409e63168cf6e9d471
roypranatosh <pranatosh.roy@hotmail.co.in>
2024-02-05 13:00:02 +0530

    all packages installed

---
+++ App.jsx
@@ -0,0 +1,24 @@
+// import 'react-native-gesture-handler';
+import React from 'react';
+// import AppNavigator from './src/AppNavigator';
+// import {store} from './src/redux/store';
+// import {Provider} from 'react-redux';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Kalabhoomi</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+export default App;
